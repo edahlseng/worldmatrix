366c4306b48d87399a7a0f95b3c9a398b38038b6
edahlseng <edahlseng@yahoo.com>
2014-08-12 17:22:10 -0400

    added a console.log

--- ui.js
+++ ui.js
@@ -648,6 +648,7 @@ function startDragging(theIFrame)
 
 	// set & remove some handlers
 	console.log(theIFrame);
+	console.log(theIframe.listeners);
 	theIFrame.removeEventListener("touchmove", iframeTouchMove);
 	theIFrame.addEventListener("touchmove", dragMove);
 	theIFrame.addEventListener("touchend", dragEnd);
